af3c166b595991cc67d59127b1fb626d0d66eff6
babakkhezry <babak.khezri7@gmail.com>
2020-03-21 23:10:03 +0430

    add time and volume

--- music.py
+++ music.py
@@ -6,63 +6,82 @@ from getpass import getuser
 from vlc import MediaPlayer
 import colorama as co
 import mutagen
-def find(): # find telegram Desktop directory
-    address = input("Enter the directory : ")
+def find(): # get the directory and make it findable
+    address = input("Welcome to my music player\nEnter the directory : ")
     address = address.split('\\')
-    address = "\\".join(address) + "\\*.mp3"
+    address = "\\".join(address) + "\\*.mp3" # just find mp3 files
     return address
 def player():
-    play = 0
-    while True:
+    #start from the first file when app start 
+    first = False
+    if first == False:
+        play = 0
+        first = True
+    while True: 
         #global file # make it global variable
         system('cls')
-        # get random mp3 file
-        show_name(play)
         lst_1 = [lst[play]]
         file = MediaPlayer(lst[play])
+        volume = file.audio_get_volume()
         file.play()
         #get the time of file
-        time = mutagen.File(lst[play])
-        time = time.info.length
-        #print(time)
-        play = change(file,play)
-        continue
-def change(file,play): # change mp3 file
-    while True:
-        change = getchar()
-        if change == "y":
-            rand = randrange(0,len(lst))
-            file.stop()
-            return rand
-        if change == "n":
-            file.stop()
-            return play + 1
-        if change == "p":
-            file.stop()
-            return play - 1
-        if change == "g":
-            while True:
-                change = input("goto : ")
-                if not change.isdigit():
-                    continue
-                if int(change) > 0 and int(change) <= len(lst):
-                    break
-            file.stop()
-            return (int(change) - 1)
-        if change == "e":
-            exit(0)
-def show_name(play):
+        time = timer(play)
+        while True:
+            show_name(play,volume,time)
+            event = getchar()
+            if event == "p" or event == "g" or event == "n" or event == "y" or event == "e" or event == "P" or event == "G" or event == "N" or event == "Y" or event == "E":
+                play = change(event,file,play)
+                break
+            if event == 'w' or event == 's' or event == 'W' or event == 'S':
+                volume = chvolume(event,file,volume)
+                file.audio_set_volume(volume)
+def change(event,file,play): # change mp3 file
+    if event == 'y':#get random file
+        rand = randrange(0,len(lst))
+        file.stop()
+        return rand
+    if event == 'n':#go to next file
+        file.stop()
+        return play + 1
+    if event == 'p':# go to pervios file
+        file.stop()
+        return play - 1
+    if event == 'g':#go to input file
+        while True:
+            go = input("goto : ")
+            #check its acceptable input
+            if not go.isdigit():
+                continue
+            if int(go) > 0 and int(go) <= len(lst):
+                break
+        file.stop()
+        return (int(go) - 1)
+    if event == 'e':
+        exit(0)
+def chvolume(event,file,vol):#change the volume
+    if event == "w":
+        return vol + 5
+    if event == "s":
+        return vol - 5  
+def show_name(play,volume,time):#print informations
+    system('cls')
     print(co.Fore.BLUE + "y = change | n = next | p = Previous | g = goto | e = exit")
     print(co.Fore.RED + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
     name = lst[play].split("\\")
     name = name[len(name)-1].split('.')
     name.pop(len(name)-1)
-    #name.remove(".mp3")
-    print(co.Fore.CYAN + "file number : {}".format(play + 1))
-    print("file name : {}".format(name[(len(name) - 1)]))
-print("Welcome to my music player")
+    print(co.Fore.GREEN + "file number : {}".format(play + 1).ljust(20),end=time.rjust(6))
+    print("\nfile name : {}".format(name[(len(name) - 1)]))
+    print("vloume : {}".format(volume))
+def timer(play):#get time of any file
+    time = mutagen.File(lst[play])
+    time = int(time.info.length)#get the files time in secound
+    minu = str(time // 60)# get minute
+    sec = str(time % 60)#get second
+    if len(sec) == 1: # make it good style
+        sec = '0' + sec
+    return minu + ":" + sec
 # get all mp3 players in telegram Desktop
 lst = glob(find())
 #start program
 player()
-#C:\Users\Babak\Downloads\Telegram Desktop
