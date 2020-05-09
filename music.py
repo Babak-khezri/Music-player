aabf3a862555314d3927801a0444de43878cc143
unknown <babak.khezri7@gamil.com>
2020-05-09 20:14:48 +0430

    Add graphic and use new algoritm

--- music.py
+++ music.py
@@ -1,87 +1,139 @@
 from glob import glob
-from tkinter import filedialog, Tk
+from tkinter import *
+from tkinter import filedialog
 from random import randrange
 from click import getchar
 from os import system, _exit
-from getpass import getuser
 from vlc import MediaPlayer
-from colorama import init, Fore, Style
 from mutagen import File
 from time import sleep
 from threading import Thread
-timer = None
-system('mode con:cols=77 lines=15')
-init(convert=True)
-print(Style.DIM + Fore.LIGHTCYAN_EX +"Welcome to my music play :\nSelect your Directory")
-event = 'none'
+timer = [1000,10]
+event = None
+box_info = ['0', '0', '0', '0']
 
-def get_file():  # Get all mp3 players in input address
+def get_files():  # Get all mp3 players in input address
+    destroy = True
     sleep(0.5)
+    welcome_mas = Tk()
+    Label(welcome_mas,text = "<<  welcome  >>\n    Please select ur directory    ",font = ("Times",25,'bold')).pack()
     while True:
-        lst = glob(find())
-        if len(lst) == 0:
-            system('cls')
-            print("Directory is empty")
-            sleep(2)
+        List_files = glob(Select_Dir())
+        if destroy == True:
+            welcome_mas.destroy()
+            destroy = False
+        tkin = Tk()
+        tkin.title("warning")
+        if len(List_files) == 0:
+            Label(tkin,text = "Directory is empty ...",font = ("Times",25,'bold')).pack()
+            sleep(1)
         else:
             Thread(target=next_music).start()
             Thread(target=add_event).start()
-            return lst
+            return List_files
 
 
-def find():  # Met the directory and make it findable
-    tkin = Tk()
-    tkin.withdraw()
+def Select_Dir():  # Met the directory and make it findable
     address = filedialog.askdirectory()
     address = address.split('\\')
     address = "\\".join(address) + "\\*.mp3"  # just find mp3 files
     return address
 
 
-def player(play):
+def graphic(): # Graphic by tkinter
+    win = Tk()
+    win.resizable(False,False)
+    win.title('music player')
+    app_icon = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\ico.png')
+    win.iconphoto(False , app_icon)
+    win['background']='#000d33'
+    Label(win, text="welcome to my music player", bg = '#000d33',fg= '#40ff00', font=("Times",24,'bold')).grid(row = 0,column=1,columnspan = 5)
+    Label(win,text = '|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|',fg = '#ffcc00',font=("Helvetica",24,'bold'),bg = '#000d33').grid(row = 1,column=0,columnspan = 50)
+    def Show_name():
+        name_file.config(text=box_info[0],fg= '#ff0000', font=("Times",18,'bold'), bg = '#000d33')
+        name_file.grid(row = 2,column=0,columnspan = 8)
+        time_file.config(text=box_info[1],fg= '#ff0000', font=("Times",20,'bold'), bg = '#000d33')
+        time_file.grid(row = 3,column=1,columnspan = 5)
+        volume.config(text=box_info[2], fg= '#ff0000', font=("Times",20,'bold'), bg = '#000d33')
+        volume.grid(row = 4,column=1,columnspan = 5)
+        number.config(text=box_info[3], fg= '#ff0000', font=("Times",20,'bold'), bg = '#000d33')
+        number.grid(row = 5,column=1,columnspan = 5)
+    def GUI_control(but):
+        global event
+        event = but
+        sleep(0.1)
+        Show_name()
+    name_file = Label(win)
+    time_file = Label(win)
+    volume = Label(win)
+    number = Label(win)
+    photo_n = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\n.png') 
+    photo_e = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\e.png') 
+    photo_b = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\b.png') 
+    photo_c = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\c.png') 
+    photo_vu = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\vu.png') 
+    photo_vd = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\vd.png') 
+    photo_pu = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\pu.png') 
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_c , command=lambda: GUI_control('c')).grid(row = 7,column=0)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_b , command=lambda: GUI_control('b')).grid(row = 7,column=2)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_pu ,command=lambda: GUI_control(' ')).grid(row = 7,column=1)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_n , command=lambda: GUI_control('n')).grid(row = 7,column=3)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_vd, command=lambda: GUI_control('w')).grid(row = 7,column=4)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_vu, command=lambda: GUI_control('s')).grid(row = 7,column=5)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_e , command=lambda: GUI_control('e')).grid(row = 7,column=6)
+    GUI_control('none')
+    win.mainloop()
+
+
+def player(play): # MAin player
+    global box_info
     global event
     event = 'none'
     play = int(play)
-    file = MediaPlayer(lst[play])
+    file = MediaPlayer(List_files[play])
     volume = file.audio_get_volume()
     file.play()
     time_file = size_file(play)
-    puse = True
-    show_name(time_file,play,volume)
-    events(file,play,volume,time_file)
+    name = List_files[play].split("\\")
+    name = name[-1].split('.')
+    name.pop(-1)
+    name = ".".join(name)
+    box_info = [name,play+1,time_file,volume]
+    commands(file, play, volume, time_file)
+    graphic()
 
 
-def events(file,play,volume,time_file):
+def commands(file, play, volume, time_file): #Check commands
     pause = True
     while True:
-        global event
+        global event , box_info
         if event == "b" or event == "g" or event == "n" or event == "c" or event == "e" or event == "B" or event == "G" or event == "N" or event == "C" or event == "E":
             file.stop()
-            change(event,play)
+            change(event, play)
         elif event == 'w' or event == 's' or event == 'W' or event == 'S':
             volume = chVolume(event, volume)
             file.audio_set_volume(volume)
             event = 'none'
-            show_name(time_file,play,volume)
+            box_info[3] = volume
         elif event == ' ':
             event = 'none'
             if pause == True:
                 file.pause()
                 pause = False
                 continue
-            else :
+            else:
                 file.play()
                 pause = True
         else:
             continue
 
 
-def change(event,play):  # Change mp3 file
+def change(event, play):  # Change mp3 file
     if event == 'c' or event == 'C':  # get random file
-        rand = randrange(0, len(lst))
+        rand = randrange(0, len(List_files))
         player(rand)
     if event == 'n' or event == 'N':  # go to next file
-        if play == len(lst) - 1:
+        if play == len(List_files) - 1:
             player(0)
         else:
             player(play + 1)
@@ -96,7 +148,7 @@ def change(event,play):  # Change mp3 file
             # check its acceptable input
             if not go.isdigit():
                 continue
-            if int(go) > 0 and int(go) <= len(lst):
+            if int(go) > 0 and int(go) <= len(List_files):
                 break
         player(int(go) - 1)
     if event == 'e' or event == 'E':
@@ -116,24 +168,11 @@ def chVolume(event, vol):  # Change the volume
             return vol
 
 
-def show_name(time_file,play,volume):  # Print informations
-    system('cls')
-    print(Fore.LIGHTMAGENTA_EX +"||C = Change | N = Next | B = Back | G = Goto | W = Vl+ | S = Vl- | E = Exit")
-    print(Fore.RED + "||-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-    name = lst[play].split("\\")
-    name = name[len(name)-1].split('.')
-    name.pop(len(name)-1)
-    name = "".join(name)
-    print(Fore.GREEN + "||File number : {}\t".format(play + 1).ljust(20) +  time_file, end = "")
-    print("\n||File name : {}".format(name))
-    print("||Vloume : {}".format(volume))
-
-
 def size_file(play):  # Get time of any file
     global timer
-    time = File(lst[play])
+    time = File(List_files[play])
     time = int(time.info.length)  # get the files time in secound
-    timer = [int(time),play]
+    timer = [int(time), play]
     minu = str(time // 60)  # get minute
     sec = str(time % 60)  # get second
     if len(sec) == 1:  # make it good style
@@ -151,7 +190,7 @@ def next_music():  # When music over go to next
             event = 'n'
 
 
-def add_event():
+def add_event(): # Add commands
     global event
     while True:
         event = getchar()
@@ -160,5 +199,12 @@ def add_event():
                 if event == 'none':
                     break
 
-lst = get_file()
+
+def Open_GUI():  # If gui closed this open it again
+    while True:
+        graphic()
+
+Thread(target = Open_GUI).start()
+List_files = get_files()
+
 player(0)
