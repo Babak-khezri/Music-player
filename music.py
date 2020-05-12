8184401e7f7d93230a0873efb425ab5fa2d97690
unknown <babak.khezri7@gamil.com>
2020-05-13 01:45:49 +0430

    Delete some extra codes

--- music.py
+++ music.py
@@ -1,5 +1,5 @@
 from glob import glob
-from tkinter import *
+from tkinter import Tk, Button, Label, mainloop, PhotoImage
 from tkinter import filedialog
 from random import randrange, choice
 from click import getchar
@@ -8,57 +8,56 @@ from vlc import MediaPlayer
 from mutagen import File
 from time import sleep
 from threading import Thread
-from emojis import encode
 from tqdm import tqdm
+
+
 timer = event = but = None
 box_info = ['','','','']
 
 
 def get_files():  # Get all mp3 players in input address
-    colors = ['red','pink','purple','orange','yellow','green','blue','white','brown']
-    destroy = True
+    colors = ['red','pink','purple','orange','yellow','green','blue','white','brown'] # Colors for warning show
+    destroy = True # Destroyed first Gui page 1 time
     sleep(0.5)
-    welcome_mas = Tk()
+    welcome_mas = Tk() # First Gui window
     massage = Label(welcome_mas,text = "<<  welcome  >>\n    Please select ur directory    ")
     massage.config(font = ("Times",25,'bold') , fg = '#000066',bg='#ffb6ff')
     massage.grid(row=0,column=0)
     while True:
-        List_files = glob(Select_Dir())
-        if destroy == True:
+        List_files = glob(Select_Dir()) # Get the Directore mp3 files
+        if destroy == True: 
             welcome_mas.destroy()
-            warining_page = Tk()
+            warining_page = Tk() # Make second Gui window that if dirctory be ampty will come up
             destroy = False
-        if len(List_files) == 0:
+        if len(List_files) == 0: # Directory is empty
             warining_page.title("warning")
             massage = Label(warining_page,text = "Directory is empty ...")
             massage.config(font = ("Times",25,'bold'), fg = choice(colors), bg = '#00004d')
             massage.grid(row=0,column=0)
             sleep(1)
-        else:
-            warining_page.destroy()
-            Thread(target = Open_GUI).start()
-            Thread(target=next_music).start()
-            Thread(target=add_event).start()
+        else: # Directory is not empty
+            warining_page.destroy() # Destroyed second Gui window
+            Thread(target = Open_GUI).start() # Open main Gui
+            Thread(target=next_music).start() # Open timer for mp3 files
             return List_files
 
 
 def Select_Dir():  # Met the directory and make it findable
     address = filedialog.askdirectory()
-    address = address.split('\\')
-    address = "\\".join(address) + "\\*.mp3"  # just find mp3 files
+    address +=  "\\*.mp3"  # just find mp3 files
     return address
 
 
 def graphic(): # Graphic by tkinter
-    win = Tk()
-    win.resizable(False,False)
+    win = Tk()  
+    win.resizable(False,False) # Lock change size
     win.title('music player')
     app_icon = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\ico.png')
-    win.iconphoto(False , app_icon)
-    win['background']='#000d33'
+    win.iconphoto(False , app_icon) # Change icon
+    win['background']='#000d33' # Change main background
     Label(win, text="welcome to my music player", bg = '#000d33',fg= '#40ff00', font=("Times",24,'bold')).grid(row = 0,column=1,columnspan = 5)
     Label(win,text = '|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|',fg = '#ffcc00',font=("Helvetica",24,'bold'),bg = '#000d33').grid(row = 1,column=0,columnspan = 50)
-    def Show_name():
+    def show_info(): # show the informations
         name_file.config(text=box_info[0],fg= '#ff0000', font=("Times",18,'bold'), bg = '#000d33')
         name_file.grid(row = 2,column=0,columnspan = 8)
         time_file.config(text=box_info[1],fg= '#ff0000', font=("Times",20,'bold'), bg = '#000d33')
@@ -67,11 +66,11 @@ def graphic(): # Graphic by tkinter
         volume.grid(row = 4,column=1,columnspan = 5)
         number.config(text=box_info[3], fg= '#ff0000', font=("Times",20,'bold'), bg = '#000d33')
         number.grid(row = 5,column=1,columnspan = 5)
-    def GUI_control(but):
+    def GUI_control(but): # Make commands
         global event
         event = but
         sleep(0.1)
-        Show_name()
+        show_info()
     name_file = Label(win)
     time_file = Label(win)
     volume = Label(win)
@@ -83,50 +82,53 @@ def graphic(): # Graphic by tkinter
     photo_vu = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\vu.png') 
     photo_vd = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\vd.png') 
     photo_pu = PhotoImage(file = 'C:\\Users\\Babak\\Desktop\\python\\EXTRA_FILES\\pu.png') 
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ch, command=lambda: GUI_control('c')).grid(row = 7,column=0)
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ba, command=lambda: GUI_control('b')).grid(row = 7,column=2)
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_pu, command=lambda: GUI_control(' ')).grid(row = 7,column=1)
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ne, command=lambda: GUI_control('n')).grid(row = 7,column=3)
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_vd, command=lambda: GUI_control('w')).grid(row = 7,column=4)
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_vu, command=lambda: GUI_control('s')).grid(row = 7,column=5)
-    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ex, command=lambda: GUI_control('e')).grid(row = 7,column=6)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ch, command=lambda: GUI_control('cha')).grid(row = 7,column=0)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ba, command=lambda: GUI_control('bac')).grid(row = 7,column=2)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_pu, command=lambda: GUI_control('pau')).grid(row = 7,column=1)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ne, command=lambda: GUI_control('nex')).grid(row = 7,column=3)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_vd, command=lambda: GUI_control('vl+')).grid(row = 7,column=4)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_vu, command=lambda: GUI_control('vl-')).grid(row = 7,column=5)
+    Button(win,height = 55, width = 60, bd=16,bg='#000099',image = photo_ex, command=lambda: GUI_control('exi')).grid(row = 7,column=6)
     GUI_control('none')
     global but
-    but = Button(win,command=lambda: GUI_control('none'))
-    but.invoke()
+    but = Button(win,command=lambda: GUI_control('none')) # The unshow but for show name when program go to next music by timer
+    but.invoke() # Put the button automatically
     win.mainloop()
 
 
 def player(play): # MAin player
     global box_info, event
-    event = 'none'
-    play = int(play)
+    event = 'none' # Avoid running a command twice
     file = MediaPlayer(List_files[play])
-    volume = file.audio_get_volume()
     file.play()
+    volume = file.audio_get_volume()
     time_file = size_file(play)
+    name = file_name(play)
+    box_info = [name,play+1,time_file,volume] # Add informations to the list for showing
+    commands(file, play, volume, time_file)
+
+
+def file_name(play): # Get the name of the file
     name = List_files[play].split("\\")
     name = name[-1].split('.')
     name.pop(-1)
-    name = ".".join(name)
-    box_info = [name,play+1,time_file,volume]
-    commands(file, play, volume, time_file)
-    graphic()
+    return ".".join(name)    
 
 
 def commands(file, play, volume, time_file): #Check commands
-    pause = True
+    global event , box_info
+    pause = True # Pause and unpa
     while True:
         global event , box_info
-        if event == "b" or event == "g" or event == "n" or event == "c" or event == "e" or event == "B" or event == "G" or event == "N" or event == "C" or event == "E":
+        if event == "bac" or event == "g" or event == "nex" or event == "cha" or event == "exi":
             file.stop()
             change(event, play)
-        elif event == 'w' or event == 's' or event == 'W' or event == 'S':
+        elif event == 'vl+' or event == 'vl-':
             volume = chVolume(event, volume)
             file.audio_set_volume(volume)
             event = 'none'
             box_info[3] = volume
-        elif event == ' ':
+        elif event == 'pau':
             event = 'none'
             if pause == True:
                 file.pause()
@@ -140,39 +142,30 @@ def commands(file, play, volume, time_file): #Check commands
 
 
 def change(event, play):  # Change mp3 file
-    if event == 'c' or event == 'C':  # get random file
+    if event == 'cha':  # get random file
         rand = randrange(0, len(List_files))
         player(rand)
-    if event == 'n' or event == 'N':  # go to next file
+    if event == 'nex':  # go to next file
         if play == len(List_files) - 1:
             player(0)
         else:
             player(play + 1)
-    if event == 'b' or event == 'B':  # go to pervios file
+    if event == 'bac':  # go to pervios file
         if play == 0:
             player(play)
         else:
             player(play - 1)
-    if event == 'g' or event == 'G':  # go to input file
-        while True:
-            go = input("||goto : ")
-            # check its acceptable input
-            if not go.isdigit():
-                continue
-            if int(go) > 0 and int(go) <= len(List_files):
-                break
-        player(int(go) - 1)
-    if event == 'e' or event == 'E':
+    if event == 'exi':
         _exit(0)
 
 
 def chVolume(event, vol):  # Change the volume
-    if event == "w" or event == 'W':
-        if vol < 350:
+    if event == 'vl+':
+        if vol < 250:
             return vol + 5
         else:
             return vol
-    if event == "s" or event == 'S':
+    if event == 'vl-':
         if vol > 0:
             return vol - 5
         else:
@@ -182,38 +175,28 @@ def chVolume(event, vol):  # Change the volume
 def size_file(play):  # Get time of any file
     global timer
     time = File(List_files[play])
-    time = int(time.info.length)  # get the files time in secound
-    timer = [int(time), play]
+    time = int(time.info.length)  # Get the files time in secound
+    timer = [int(time), play] # Send it for timer function
     minu = str(time // 60)  # get minute
     sec = str(time % 60)  # get second
-    if len(sec) == 1:  # make it good style
+    if len(sec) == 1:  # make it final style
         sec = '0' + sec
-    return "< " + minu + ":" + sec + " >"
+    return ("< " + minu + ":" + sec + " >")
 
 
 def next_music():  # When music over go to next
     global event, but
-    while True:
+    while True: # A timer
         sleep(1)
         timer[0] -= 1
         if timer[0] == -1:
             timer[0] -= 1
-            event = 'n'
-            sleep(0.1)
-            but.invoke()
+            event = 'nex'
+            sleep(0.1) # Get time to informations change
+            but.invoke() # Push to extra button for showing informations
 
 
-def add_event(): # Add commands
-    global event
-    while True:
-        event = getchar()
-        if event == 'g':
-            while True:
-                if event == 'none':
-                    break
-
-
-def Open_GUI():  # If gui closed this open it again
+def Open_GUI():  # If GUI closed this open it again
     while True:
         graphic()
 
